2e35b1e30527d6335696c1d17084363ea97608bc
Claudia Pellegrino <claui@users.noreply.github.com>
2023-01-11 19:14:30 +0100

    Expose API surface

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -89,7 +89,7 @@ export async function activate(context: ExtensionContext) {
     title: "Show extension log",
   };
 
-  return {};
+  return { ifm } as { ifm: Ifm };
 }
 
 export function deactivate() {}
