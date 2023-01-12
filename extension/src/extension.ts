bfb1777451a2e13ab3b685945c597c3c116d5961
Claudia Pellegrino <claui@users.noreply.github.com>
2023-01-12 16:46:42 +0100

    Extract class

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,21 +1,17 @@
 import {
   commands,
-  DocumentSelector,
   ExtensionContext,
-  languages,
-  LanguageStatusItem,
-  LanguageStatusSeverity,
+  OutputChannel,
   window,
 } from "vscode";
 
-import { Ifm, IfmCli } from "./cli-api";
+import { Ifm } from "./cli-api";
 import { IfmAdapter } from "./cli-api/impl";
-import CliFailedError from "./errors";
 import Logger from "./logger";
+import { Status } from "./status";
 import { getCurrentTimestamp } from "./time";
 
-const outputChannel = window.createOutputChannel("IFM");
-const languageSelector: DocumentSelector = { language: "ifm" };
+const channel: OutputChannel = window.createOutputChannel("IFM");
 
 const log: Logger = {
   debug: function (...args) {
@@ -28,57 +24,17 @@ const log: Logger = {
     this.log("INFO", ...args);
   },
   log: function (level: string, ...args: any[]) {
-    const timestamp = getCurrentTimestamp();
-    outputChannel.appendLine(`${timestamp} [${level}] ${args.join(" ")}`);
+    channel.appendLine(`${getCurrentTimestamp()} [${level}] ${args.join(" ")}`);
   },
-};
-
-const statusItem: LanguageStatusItem = languages.createLanguageStatusItem(
-  "ifm.status.item.version",
-  languageSelector
-);
-
-const refreshStatus = async (ifmCli: IfmCli) => {
-  try {
-    statusItem.text = "Querying IFM CLI version";
-    log.info(statusItem.detail);
-    statusItem.busy = true;
-
-    const versionNumber: string = await ifmCli.version;
-    statusItem.text = `IFM CLI v${versionNumber}`;
-    statusItem.severity = LanguageStatusSeverity.Information;
-    log.info(statusItem.text);
-    statusItem.detail = `Last updated: ${getCurrentTimestamp()}`;
-  } catch (error) {
-    log.error(error?.message ?? error);
-    statusItem.text = error.message;
-    if (error instanceof CliFailedError && "cause" in error) {
-      statusItem.detail = `Caused by: ${error.cause}`;
-      log.error(`> ${error.cause}`);
-    } else {
-      statusItem.detail = undefined;
-    }
-    statusItem.severity = LanguageStatusSeverity.Error;
-  } finally {
-    statusItem.busy = false;
-  }
 };
 
 export async function activate(context: ExtensionContext) {
   const ifm: Ifm = await IfmAdapter.newInstance(log);
-  refreshStatus(ifm.cli);
-  ifm.onDidCliChange(() => refreshStatus(ifm.cli), this);
+  const status: Status = new Status(ifm, log);
+  status.refresh();
 
-  commands.registerCommand("ifm.action.refresh", () => {
-    refreshStatus(ifm.cli);
-  });
-  commands.registerCommand("ifm.action.showLog", () => {
-    outputChannel.show();
-  });
-  statusItem.command = {
-    command: "ifm.action.showLog",
-    title: "Show extension log",
-  };
+  commands.registerCommand("ifm.action.refresh", status.refresh, status);
+  commands.registerCommand("ifm.action.showLog", channel.show, channel);
 
   return { ifm };
 }
