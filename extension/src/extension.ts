f9108acd2fa9288f239b9c3d068d98a37cf5839f
Claudia Pellegrino <claui@users.noreply.github.com>
2023-01-13 13:51:53 +0100

    Use proper immutable key for map

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -11,10 +11,16 @@ import { IfmAdapter } from "./cli-api/impl";
 import log from "./log";
 import { Status } from "./status";
 
-const diagnosticsByUri: Map<Uri, DiagnosticCollection> = new Map();
+const diagnosticsByUri: Map<string, DiagnosticCollection> = new Map();
 
 function isSchemeRelevant(uri: Uri) {
-  return !["git", "gitfs", "output"].includes(uri.scheme);
+  const isExcluded: boolean = ["git", "gitfs", "output"].includes(uri.scheme);
+  if (!isExcluded) {
+    log.debug("Checking URI:", uri.toString());
+    log.debug("Scheme:", uri.scheme);
+    log.debug("Verdict:", !isExcluded);
+  }
+  return !isExcluded;
 }
 
 function updateDiagnostics(
@@ -28,7 +34,7 @@ function updateDiagnostics(
 export async function activate() {
   const ifm: IfmAdapter = await IfmAdapter.newInstance();
   const status: Status = new Status(ifm);
-  status.refresh();
+  await status.refresh();
 
   commands.registerCommand("ifm.action.refresh", ifm.refreshCli, ifm);
   commands.registerCommand("ifm.action.showLog", log.show, log);
@@ -37,9 +43,14 @@ export async function activate() {
     if (!isSchemeRelevant(event.document.uri)) {
       return;
     }
-    const diagnostics = diagnosticsByUri.get(event.document.uri);
+    const diagnostics: DiagnosticCollection | undefined = diagnosticsByUri.get(
+      event.document.uri.toString()
+    );
     if (!diagnostics) {
       log.warn("Missing DiagnosticCollection:", event.document.uri.toString());
+      const diagnostics: DiagnosticCollection =
+        languages.createDiagnosticCollection(event.document.uri.toString());
+      diagnosticsByUri.set(event.document.uri.toString(), diagnostics);
       return;
     }
     updateDiagnostics(
@@ -52,26 +63,40 @@ export async function activate() {
     if (!isSchemeRelevant(document.uri)) {
       return;
     }
-    if (diagnosticsByUri.has(document.uri)) {
+    if (diagnosticsByUri.has(document.uri.toString())) {
       log.warn("Found orphaned DiagnosticCollection:", document.uri.toString());
-      diagnosticsByUri.delete(document.uri);
+      diagnosticsByUri.delete(document.uri.toString());
     }
     const diagnostics = languages.createDiagnosticCollection(
       document.uri.toString()
     );
-    diagnosticsByUri.set(document.uri, diagnostics);
+    diagnosticsByUri.set(document.uri.toString(), diagnostics);
     updateDiagnostics(document.uri, diagnostics, "onDidOpenTextDocument");
   });
   workspace.onDidCloseTextDocument((document) => {
     if (!isSchemeRelevant(document.uri)) {
       return;
     }
-    if (!diagnosticsByUri.has(document.uri)) {
+    if (!diagnosticsByUri.has(document.uri.toString())) {
       log.warn("Missing DiagnosticCollection:", document.uri.toString());
       return;
     }
-    diagnosticsByUri.delete(document.uri);
+    diagnosticsByUri.delete(document.uri.toString());
+    log.debug("Diagnostics deleted");
   });
+  for (const document of workspace.textDocuments) {
+    if (
+      !isSchemeRelevant(document.uri) ||
+      diagnosticsByUri.has(document.uri.toString())
+    ) {
+      continue;
+    }
+    const diagnostics = languages.createDiagnosticCollection(
+      document.uri.toString()
+    );
+    diagnosticsByUri.set(document.uri.toString(), diagnostics);
+    updateDiagnostics(document.uri, diagnostics, "initial state of workspace");
+  }
 
   return { ifm } as { ifm: Ifm };
 }
