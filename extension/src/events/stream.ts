9d74786a623cafb91acf5bbbcdf5631e632b2223
Claudia Pellegrino <claui@users.noreply.github.com>
2023-02-23 21:58:54 +0100

    Simplify, refactor, document

--- extension/src/events/stream.ts
+++ extension/src/events/stream.ts
@@ -6,19 +6,25 @@ interface EventStreamFunction<T, U, A extends Arr> {
   (...args: [...A, Event<T>]): Event<U>;
 }
 
-interface EventStream<T> extends Event<T> {
+export class EventStream<T> {
+  event: Event<T>;
+
+  map<U>(fn: (e: Event<T>) => Event<U>): EventStream<U> & Event<U> {
+    return EventStream.of(fn(this.event));
+  }
+
   through<U, A extends Arr>(
     fn: EventStreamFunction<T, U, A>,
     ...args: A
-  ): EventStream<U>;
-}
+  ): EventStream<U> & Event<U> {
+    return EventStream.of(fn(...args, this.event));
+  }
 
-export function streamEvents<T>(event: Event<T>): EventStream<T> {
-  const stream: EventStream<T> = function (...args) {
-    return event(...args);
-  };
-  stream.through = (fn, ...args) => {
-    return streamEvents(fn(...args, event));
-  };
-  return stream;
+  static of<T>(event: Event<T>): EventStream<T> & Event<T> {
+    const result: EventStream<T> & Event<T> = (...args) => event(...args);
+    result.map = EventStream.prototype.map;
+    result.through = EventStream.prototype.through;
+    result.event = event;
+    return result;
+  }
 }
